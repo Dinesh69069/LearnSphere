2b881749a4488ee7be9540833f42e8c208516c8c
Ashutosh Pradhan <ashutoshcs054@gmail.com>
2025-12-13 03:13:32 +0530

    Sign up , log in

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,16 @@ module.exports = {
     "./shared/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand': {
+          'bg': 'hsl(190, 95%, 95%)',
+          'fg': 'hsl(190, 95%, 10%)',
+          'primary': 'hsl(190, 95%, 45%)',
+          'secondary': 'hsl(270, 80%, 60%)',
+        }
+      }
+    },
   },
   plugins: [],
 }
